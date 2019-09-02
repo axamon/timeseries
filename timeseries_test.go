f7c634d52ad1bb450a3cd40f928067463fda0a92
Alberto Bregliano <alberto.bregliano@pm.me>
2019-09-02 09:42:42 +0200

    added benchmark

--- timeseries_test.go
+++ timeseries_test.go
@@ -5,6 +5,7 @@
 package timeseries_test
 
 import (
+	"math/rand"
 	"reflect"
 	"testing"
 	"time"
@@ -28,13 +29,49 @@ func TestTimeseries_AddNewPoint(t *testing.T) {
 	}{
 		{"Primo", ts, args{v: 50, x: int64(10)}, 50, false},
 		{"Secondo", ts, args{v: 50, x: int64(8)}, 40, true},
+		//{"no", ts, args{v: 50, x: "otto"}, 40, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.ts.AddNewPoint(tt.args.v, tt.args.x)
+			err := tt.ts.AddNewPoint(tt.args.v, tt.args.x)
+			if err != nil {
+				t.Errorf("Timeseries.AddNewPoint() wantErr %v, expected %v, got error: %v", tt.wantErr, tt.expected, err)
+			}
 			ok := tt.expected != ts.XY[tt.args.x.(int64)]
 			if ok != tt.wantErr {
-				t.Errorf("Timeseries.AddNewPoint() wantErr %v, expected %v, got %v", tt.wantErr, tt.expected, ts.XY[tt.args.x.(int64)])
+				t.Errorf("Timeseries.AddNewPoint() wantErr %v, expected %v, got error: %v", tt.wantErr, tt.expected, err)
+			}
+		})
+	}
+}
+
+func TestTimeseries_AddValueToIndex(t *testing.T) {
+	var ts = timeseries.New()
+
+	type args struct {
+		v float64
+		x int64
+	}
+	tests := []struct {
+		name     string
+		ts       *timeseries.Timeseries
+		args     args
+		expected float64
+		wantErr  bool
+	}{
+		{"Primo", ts, args{v: 50, x: int64(10)}, 50, false},
+		{"Secondo", ts, args{v: 50, x: int64(8)}, 40, true},
+		{"terzo", ts, args{v: 50, x: int64(10)}, 100, false},
+		{"quarto", ts, args{v: -70, x: int64(10)}, 30, false},
+		{"quarto", ts, args{v: -40, x: int64(10)}, 10, true},
+		{"quinto", ts, args{v: 0, x: int64(10)}, -10, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.ts.AddValueToIndex(tt.args.v, tt.args.x)
+			ok := tt.expected != ts.XY[tt.args.x]
+			if ok != tt.wantErr {
+				t.Errorf("Timeseries.AddNewPoint() wantErr %v, expected %v, got %v", tt.wantErr, tt.expected, ts.XY[tt.args.x])
 			}
 		})
 	}
@@ -79,5 +116,13 @@ func TestTimeseries_ToSlice(t *testing.T) {
 				t.Errorf("Timeseries.ToSlice() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkAddNewPoint(b *testing.B) {
+	ts := timeseries.New()
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		ts.AddNewPoint(float64(rand.Intn(1000)), time.Now().Add(time.Duration(n)*time.Hour))
 	}
 }
