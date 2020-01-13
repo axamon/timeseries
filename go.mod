b11187fe9617df24579eabb782d410aaec63e8a8
Alberto Bregliano <alberto.bregliano@pm.me>
2020-01-13 01:07:25 +0100

    ok

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/axamon/timeseries
+
+go 1.13
---
+++ types.go
@@ -0,0 +1,20 @@
+package timeseries
+
+import "sync"
+
+// Point rappresent a point present in a time serie.
+type Point struct {
+	X int64
+	Y float64
+}
+
+// Timeseries is the type for time series.
+type Timeseries struct {
+	XY           map[int64]float64
+	orderedIndex []int64
+	firstX       int64
+	lastX        int64
+	firstY       float64
+	lastY        float64
+	sync.Mutex
+}
